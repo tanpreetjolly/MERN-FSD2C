3949b0df6b67fba344492bc3e30843418993db4e
Kshitij <kshitij.tanwar2002@gmail.com>
2024-05-08 20:29:45 +0530

    Feat: index.js updation and Register feature

---
+++ react-jobs-app-main/src/pages/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Register from './Register';
+import Home from './Home';
+import Dashboard from './Dashboard';
+import Edit from './Edit';
+import Error from './Error';
+import PrivateRoute from './PrivateRoute';
+export { Home, Register, Dashboard, Edit, Error, PrivateRoute };
