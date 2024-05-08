14245a060ae842b7bd3abb2698b7256b01bf5378
Vedant <vedantnagar21@gmail.com>
2024-05-08 16:26:04 +0530

    edited auth controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,12 +4,10 @@ const { BadRequestError, UnauthenticatedError } = require('../errors');
 
 const register = async (req, res) => {
   const user = await User.create({ ...req.body }); //creating a user
-  //...tempUser creates a new object by spreading the properties and values of tempUser. It has the same properties as of tempUser.
-  //This new object is used as an argument to the User.create() method to provide the necessary data for creating a new user.
 
   const token = user.createJWT();
 
-  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token }); //Respond with user's name and the generated JWT token
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token });
 };
 
 const login = async (req, res) => {
